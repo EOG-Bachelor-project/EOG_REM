909d2792edf7932e8bc74f4f0497163c2c9131b5
adamk1308 <adam.blichert.klovborg@regionh.dk>
2026-02-20 14:57:47 +0100

    Created file to test loading of data

---
+++ TEST_LOAD_EDF.py
@@ -0,0 +1,11 @@
+# TEST_LOAD_EDF.py
+
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import mne
+
+file_path = "L:\Auditdata\RBD PD\PD-RBD Glostrup Database_ok\DCSM_1_a"
+
+def load_files():
+    return None
