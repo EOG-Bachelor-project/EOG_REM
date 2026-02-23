349507e5c923e73fd5a2370fced8afddc2842e68
adamk1308 <adam.blichert.klovborg@regionh.dk>
2026-02-23 10:56:32 +0100

    Made updates to the environment so pytorch is set to a verison that should work with GSSC

--- GSSC_test.py
+++ GSSC_test.py
@@ -5,12 +5,16 @@
 # – - – - – - – - – - – - – - – - – - – - – - – - – - – - – - – - – - –
 from __future__ import annotations
 
-from pathlib import Path
-import numpy as np
-import pandas as pd
-import mne
+import torch
+import gssc.networks
+
+torch.serialization.add_safe_globals([gssc.networks.ResSleep])
 
 from gssc.infer import EEGInfer
+from pathlib import Path
+import mne
+
+
 
 # ≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠
 # Predefined variables
@@ -42,7 +46,7 @@ def test_GSSC(folder_path: str):
     infer = EEGInfer()
 
     # Run inferencer
-    hypnogram = infer.mne_infer(raw)
+    hypnogram = infer.mne_infer(inst=raw)
     print(hypnogram)
 
     return hypnogram
@@ -52,3 +56,8 @@ def test_GSSC(folder_path: str):
 # – - – - – - – - – - – - – - – - – - – - – - – - – - – - – - – - – - –
 
 test_GSSC(Path("L:\Auditdata\RBD PD\PD-RBD Glostrup Database_ok\DCSM_1_a\contiguous.edf"))
+
+# OUTPUT
+# (BPML) PS C:\Users\AKLO0022\EOG_REM> & C:/Users/AKLO0022/AppData/Local/anaconda3/envs/BPML/python.exe c:/Users/AKLO0022/EOG_REM/GSSC_test.py
+#OMP: Error #15: Initializing libomp.dll, but found libiomp5md.dll already initialized.
+#OMP: Hint This means that multiple copies of the OpenMP runtime have been linked into the program. That is dangerous, since it can degrade performance or cause incorrect results. The best thing to do is to ensure that only a single OpenMP runtime is linked into the process, e.g. by avoiding static linking of the OpenMP runtime in any library. As an unsafe, unsupported, undocumented workaround you can set the environment variable KMP_DUPLICATE_LIB_OK=TRUE to allow the program to continue to execute, but that may cause crashes or silently produce incorrect results. For more information, please see http://openmp.llvm.org/
