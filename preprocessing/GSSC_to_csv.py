c2a6aa7889c6a2a350d5dec385a108f928b8db13
s234994 <s234994@dtu.dk>
2026-03-09 13:47:18 +0100

    Added rename step in GSSC to csv and created csv to check output of check rems

--- preprocessing/GSSC_to_csv.py
+++ preprocessing/GSSC_to_csv.py
@@ -46,20 +46,33 @@ def test_GSSC(folder: str | Path):
     print("Channels:", raw.ch_names[:20], "..." if len(raw.ch_names) > 20 else "")
     print("sfreq:", raw.info["sfreq"])
 
-    # 2) Pick channels
+    # 2) Rename EOG channels 
+    rename_map = build_rename_map(raw.ch_names)
+    print("Rename map:", rename_map)
+
+    if rename_map:
+        raw.rename_channels(rename_map)
+
+    # Check if both LOC and ROC channels are present after renaming
+    missing = [ch for ch in ["LOC", "ROC"] if ch not in raw.ch_names]
+    if missing:
+        print(f"Skipping {edf_path.name} - missing channels: {missing}")
+        return
+
+    # 3) Pick channels
     picks = ["LOC", "ROC"]
     missing = [ch for ch in picks if ch not in raw.ch_names]
     if missing:
         raise ValueError(f"Missing expected channels: {missing}. Available: {raw.ch_names}")
     raw.pick(picks)
 
-    # 3) Load data (Required for GSSC)
+    # 4) Load data (Required for GSSC)
     raw.load_data()
 
-    # 4) Set channel types (Helps GSSC choose)
+    # 5) Set channel types (Helps GSSC choose)
     raw.set_channel_types({"ROC": "eog", "LOC": "eog"})
 
-    # 5) Run inference
+    # 6) Run inference
     infer = EEGInfer()
     stages, times, probs = infer.mne_infer(inst=raw)
     
@@ -69,11 +82,13 @@ def test_GSSC(folder: str | Path):
          })
     # Add probalities to dataframe and rename stages to string
     df[["P_W", "P_N1", "P_N2", "P_N3", "P_REM"]] = probs
-    df['stages'] = df['stages'].map({0: 'W', 1: 'N1', 2: 'N2', 3: 'N3', 4: 'REM'})
+    df['Stages'] = df['Stages'].map({0: 'W', 1: 'N1', 2: 'N2', 3: 'N3', 4: 'REM'})
+    
 
     print(df)
-    # 6) Convert datframe to csv and save it 
+    # 7) Convert datframe to csv and save it 
     df.to_csv('gssc_results.csv', index=False)
     print("Saved: gssc_results.csv")
 
     return stages, times, probs
+
