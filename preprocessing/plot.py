9f384a9ac6891bad44415e830895d4a89b7588fa
Adam Blichert Klovborg <s223663@dtu.dk>
2026-03-13 09:44:59 +0100

    Added save figure to the 'plot_eog_signals'

--- preprocessing/plot.py
+++ preprocessing/plot.py
@@ -16,7 +16,10 @@ from pathlib import Path
 # 1 ————————————————————————————————————————————————————————————————————————
 # 1 Function to plot EOG signals
 # 1 ————————————————————————————————————————————————————————————————————————
-def plot_eog_signals(eog_file: str) -> None:
+def plot_eog_signals(eog_file: str, 
+                     Lcolor: str = "blue", 
+                     Rcolor: str = "red",
+                     save_path: str = None) -> None:
     """
     Plots EOG signals from a CSV file.
 
@@ -24,6 +27,12 @@ def plot_eog_signals(eog_file: str) -> None:
     ----------
     eog_file : str
         The path to the EOG CSV file.
+    Lcolor : str
+        The color for the LOC signal.
+    Rcolor : str
+        The color for the ROC signal.
+    save_path : str
+        Optional path to save the plot image. If None, the plot will be displayed instead.
     """
 
     # 1) Load EOG CSV and check for required columns
@@ -33,14 +42,20 @@ def plot_eog_signals(eog_file: str) -> None:
     
     # 2) Plot LOC and ROC signals over time
     plt.figure(figsize=(15, 5), dpi=2000)
-    plt.plot(eog_df["time_sec"], eog_df["LOC"], label="LOC", color="blue") # Plot LOC signal
-    plt.plot(eog_df["time_sec"], eog_df["ROC"], label="ROC", color="red")  # Plot ROC signal
+    plt.plot(eog_df["time_sec"], eog_df["LOC"], label="LOC", color=Lcolor) # Plot LOC signal
+    plt.plot(eog_df["time_sec"], eog_df["ROC"], label="ROC", color=Rcolor) # Plot ROC signal
     plt.xlabel("Time [s]")                                                 # Label for x-axis
     plt.ylabel("Amplitude")                                                # Label for y-axis
     plt.title("EOG Signals")                                               # Title for the plot
     plt.legend()                                                           # Legend
     plt.grid()                                                             # Grid for better visibility
-    plt.tight_layout(); plt.show()
+    plt.tight_layout()
+
+    # 3) Save or show the plot
+    if save_path:
+        plt.savefig(save_path, dpi=2000)
+    else:
+        plt.show()
 
 # 2 ————————————————————————————————————————————————————————————————————————
 # 2 Function to plot EOG signals with GSSC staging
