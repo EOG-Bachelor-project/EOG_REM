26c53521eeed8631e738a6815bb2f13062b7f7fd
s234994 <s234994@dtu.dk>
2026-04-07 11:18:26 +0200

    Added file to test extracting eeg signals from the eog signals

---
+++ Tests/test_eeg_signals_from_eog.py
@@ -0,0 +1,28 @@
+# Filename: test_eeg_signals_from_eog.py
+# Authors: Adam Klovborg & Rasmus Kleffel
+# Description: File to test extracting eeg signals from EM_detect by subtracting mask applied in extract_rems.py provided by our main supervisor Andreas Brink-Kjaer.
+
+# =====================================================================
+# Imports
+# =====================================================================
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from extract_rems import detect_rem_jaec 
+import pandas as pd
+
+
+def eeg_signals_from_eog():
+
+    mask = None # mask from extract_rems
+    
+
+
+
+
+
+
+
+
+    return None 
